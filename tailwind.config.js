a554e8b30d1bad7faad53ef33194de4e9ffdaaf8
Seraphin <svandegar@hotmail.com>
2022-02-18 17:33:41 -0400

    SETUP - Set theme for code inline and block

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
 
 module.exports = {
   experimental: {
@@ -22,13 +21,34 @@ module.exports = {
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: colors.teal,
-        gray: colors.neutral,
+        primary: {
+          0: '#D4DADD',
+          100: '#A8B5BA',
+          200: '#7D9098',
+          300: '#516B75',
+          400: '#264653',
+          500: '#1E3842',
+          600: '#172A32',
+          700: '#0F1C21',
+          800: '#080E11',
+        },
+        secondary: {
+          0: '#FAE2DC',
+          100: '#F5C5B9',
+          200: '#F1A997',
+          300: '#EC8C74',
+          400: '#E76F51',
+          500: '#B95941',
+          600: '#8B4331',
+          700: '#5C2C20',
+          800: '#2E1610',
+        },
+        // gray: colors.neutral,
       },
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: theme('colors.gray.700'),
+            color: theme('colors.secondary.700'),
             a: {
               color: theme('colors.primary.500'),
               '&:hover': {
@@ -39,26 +59,27 @@ module.exports = {
             h1: {
               fontWeight: '700',
               letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.900'),
+              color: theme('colors.secondary.900'),
             },
             h2: {
               fontWeight: '700',
               letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.900'),
+              color: theme('colors.secondary.900'),
             },
             h3: {
               fontWeight: '600',
-              color: theme('colors.gray.900'),
+              color: theme('colors.secondary.900'),
             },
             'h4,h5,h6': {
-              color: theme('colors.gray.900'),
+              color: theme('colors.secondary.900'),
             },
             pre: {
-              backgroundColor: theme('colors.gray.800'),
+              // Code blocks background
+              backgroundColor: theme('colors.primary.600'),
             },
             code: {
-              color: theme('colors.pink.500'),
-              backgroundColor: theme('colors.gray.100'),
+              color: theme('colors.primary.400'),
+              backgroundColor: theme('colors.secondary.200'),
               paddingLeft: '4px',
               paddingRight: '4px',
               paddingTop: '2px',
@@ -72,31 +93,31 @@ module.exports = {
               content: 'none',
             },
             details: {
-              backgroundColor: theme('colors.gray.100'),
+              backgroundColor: theme('colors.secondary.100'),
               paddingLeft: '4px',
               paddingRight: '4px',
               paddingTop: '2px',
               paddingBottom: '2px',
               borderRadius: '0.25rem',
             },
-            hr: { borderColor: theme('colors.gray.200') },
+            hr: { borderColor: theme('colors.secondary.200') },
             'ol li::marker': {
               fontWeight: '600',
-              color: theme('colors.gray.500'),
+              color: theme('colors.secondary.500'),
             },
             'ul li::marker': {
-              backgroundColor: theme('colors.gray.500'),
+              backgroundColor: theme('colors.secondary.500'),
             },
-            strong: { color: theme('colors.gray.600') },
+            strong: { color: theme('colors.secondary.600') },
             blockquote: {
-              color: theme('colors.gray.900'),
-              borderLeftColor: theme('colors.gray.200'),
+              color: theme('colors.secondary.900'),
+              borderLeftColor: theme('colors.secondary.200'),
             },
           },
         },
         dark: {
           css: {
-            color: theme('colors.gray.300'),
+            color: theme('colors.secondary.300'),
             a: {
               color: theme('colors.primary.500'),
               '&:hover': {
@@ -107,51 +128,53 @@ module.exports = {
             h1: {
               fontWeight: '700',
               letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.100'),
+              color: theme('colors.secondary.100'),
             },
             h2: {
               fontWeight: '700',
               letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.100'),
+              color: theme('colors.secondary.100'),
             },
             h3: {
               fontWeight: '600',
-              color: theme('colors.gray.100'),
+              color: theme('colors.secondary.100'),
             },
             'h4,h5,h6': {
-              color: theme('colors.gray.100'),
+              color: theme('colors.secondary.100'),
             },
             pre: {
-              backgroundColor: theme('colors.gray.800'),
+              // Code blocks background
+              backgroundColor: theme('colors.primary.600'),
             },
             code: {
-              backgroundColor: theme('colors.gray.800'),
+              color: theme('colors.secondary.400'),
+              backgroundColor: theme('colors.primary.600'),
             },
             details: {
-              backgroundColor: theme('colors.gray.800'),
+              backgroundColor: theme('colors.secondary.800'),
             },
-            hr: { borderColor: theme('colors.gray.700') },
+            hr: { borderColor: theme('colors.secondary.700') },
             'ol li::marker': {
               fontWeight: '600',
-              color: theme('colors.gray.400'),
+              color: theme('colors.secondary.400'),
             },
             'ul li::marker': {
-              backgroundColor: theme('colors.gray.400'),
+              backgroundColor: theme('colors.secondary.400'),
             },
-            strong: { color: theme('colors.gray.100') },
+            strong: { color: theme('colors.secondary.100') },
             thead: {
               th: {
-                color: theme('colors.gray.100'),
+                color: theme('colors.secondary.100'),
               },
             },
             tbody: {
               tr: {
-                borderBottomColor: theme('colors.gray.700'),
+                borderBottomColor: theme('colors.secondary.700'),
               },
             },
             blockquote: {
-              color: theme('colors.gray.100'),
-              borderLeftColor: theme('colors.gray.700'),
+              color: theme('colors.secondary.100'),
+              borderLeftColor: theme('colors.secondary.700'),
             },
           },
         },
