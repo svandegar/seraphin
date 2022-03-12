c680ac5c32d146e9b850a2abd6bd2bbcf9da5c21
Seraphin <svandegar@hotmail.com>
2022-03-12 11:41:07 -0400

    IMAGES - Attempt to fix project images

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,13 +2,13 @@ const projectsData = [
   {
     title: 'La Bibli',
     description: `Bibliothèque en ligne. Partagez vos livres simplement.`,
-    imgSrc: '/static/images/labibli.png',
+    imgSrc: '/static/images/labibli_2.png',
     href: 'https://labibli.com',
   },
   {
     title: 'Feuille de temps',
     description: `Solution de gestion de feuilles de temps pour le secteur communautaire canadien.`,
-    imgSrc: '/static/images/feuilledetemps.png',
+    imgSrc: '/static/images/feuilledetemps.jpg',
     href: 'https://feuilledetemps.ca',
   },
 ]
